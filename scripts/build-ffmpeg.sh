8f0f0629a21d257b0ab51f3db39f041453298e63
lysora <lysorax@gmail.com>
2022-06-18 12:54:21 +0800

    Enable dxva2 decoder

--- scripts/build-ffmpeg.sh
+++ scripts/build-ffmpeg.sh
@@ -11,11 +11,11 @@ git clone https://git.ffmpeg.org/ffmpeg.git --depth 1 -b $TAG && cd ffmpeg || ex
 ./configure \
     --disable-all \
     --enable-avcodec \
+    --enable-dxva2 \
     --enable-decoder=h264 \
     --enable-decoder=hevc \
-    --enable-hwaccel=h264_vaapi \
-    --enable-hwaccel=hevc_vaapi \
-    --enable-dxva2 \
+    --enable-hwaccel=h264_dxva2 \
+    --enable-hwaccel=hevc_dxva2 \
     --prefix="$ROOT/ffmpeg-prefix" "$@" \
     || exit 1
 make -j4 || exit 1
